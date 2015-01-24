5c50b98ace7d42ff25000f536efc7a811a6176e3
lexiaoxi <bianlongfei@henmatoMacBook-Pro.local>
2015-01-24 19:50:17 +0800

    form-ayto、table-grid、menubar、tab的小ui

---
+++ js/menu.js
@@ -0,0 +1,69 @@
+
+(function($){
+	$.fn.menuBar=function(options){
+		var settings=$.extend({
+			items:[],
+			width:100,
+			speed:600
+		},options);
+		return this.each(function(){
+			var self=$(this),
+				menuLength=settings.items.length,
+				menuDiv='';
+			var control={
+				render:function(){
+					for(i=0;i<menuLength;i++){
+						menuDiv+="<div class='menuItem'><span>"+settings.items[i].text+"</span><ul class='menuBox'></ul></div>";
+					}
+					self[0].innerHTML=menuDiv;
+					//self.append(menuDiv);
+					for(i=0;i<menuLength;i++){
+						var menuList='';
+						if(settings.items[i].menu){
+							listLen=settings.items[i].menu.list.length;
+							var childMenu=settings.items[i].menu;
+							for(var k=0;k<listLen;k++){
+								menuList+='<li><a href="'+childMenu.list[k].url+'">'+childMenu.list[k].text+'</a></li>';
+							}
+							$('.menuItem').eq(i).find("ul")[0].innerHTML=menuList;
+							//$('.menuItem').eq(i).find("ul").append(menuList);	
+							//console.log($('.menuItem').eq(i).find("ul")[0]);
+						}
+					}
+				},
+				show:function(item){
+					self.find('div.menuItem').hover(function(){
+						$(this).find('ul').width(settings.width).stop(true,false).show(settings.speed);						
+					},function(){
+						$(this).find('ul').hide();
+					});
+				},
+				init:function(){
+					this.render();
+					this.show();
+				}
+			}
+			control.init();
+		})
+	}
+})(jQuery);
+
+
+function fn(arg1, arg2, callback){
+    var num = Math.ceil(Math.random() * (arg1 - arg2) + arg2);
+    callback(num);　　//传递结果
+}
+
+fn(10, 20, function(num){
+   console.log("Callback called! Num: " + num); 
+});　　
+
+function cc(num){
+   console.log("Callback called! Num: " + num); 
+}
+function fn(arg1, arg2, callback){
+    var num = Math.ceil(Math.random() * (arg1 - arg2) + arg2);
+    callback(num);　　//传递结果
+}
+
+fn(10, 20,cc );　
---
+++ js/tab.js
@@ -0,0 +1,67 @@
+
+(function($){
+	$.fn.extend({
+		tab:function(options){
+			var defaults={
+				closeW:true,//可关闭tab
+				closeAll:true,//可关闭所有tab
+				height:"auto",//tab内容高度
+				width:"auto",//tab内容宽度
+				k:0
+			};
+			var options=$.extend(defaults,options);
+			return this.each(function(){
+				var o=options,
+					$self=$(this),
+					len=$self.find("li").length,
+					tabList=$self.find("li").find("span"),
+					closeIcon=$self.find("li").find("i"),
+					tabContent=$self.siblings();
+				var control={
+					//tab关闭
+					tabClose:function(){
+						if(o.closeW){
+							closeIcon.show();
+							if(o.closeAll){
+								closeIcon.show();
+							}else{
+								closeIcon.eq(0).hide();
+							}
+							closeIcon.click(function(){
+								var index=$(this).parent().index();
+								if($(this).parent().hasClass("on")){
+									$(this).parent().removeClass("on").next().addClass("on");
+									tabContent.children().eq(index).next().show();
+									var currentLen=$self.find("li").length;
+									if(index==currentLen-1){
+										$(tabContent).children().eq(index).siblings().eq(0).show();
+										$(this).parent().siblings().eq(0).addClass("on");
+									}
+									tabContent.children().eq(index).remove();
+								}else{
+									tabContent.children().eq(index).remove();
+								}
+								$(this).parent().remove();
+							})
+						}
+					},
+					//tab切换
+					change:function(){
+						tabList.click(function(){
+							var index=$(this).parent().index();
+							tabContent.children().height(o.height).width(o.width);
+							tabContent.children().eq(index).show().siblings().hide();
+							tabList.parent().removeClass("on");
+							$(this).parent().addClass("on");
+						}).eq(o.k).click();	
+					},
+					init:function(){
+						this.change();
+						this.tabClose();
+					}
+				}
+				control.init();	
+			})
+		}
+	})
+})(jQuery);
